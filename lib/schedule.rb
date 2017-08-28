4f8e2d4f23889f1abd2076a009fbb9f095ad90c4
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2017-08-27 19:42:59 -0500

    code review proposals

--- lib/schedule.rb
+++ lib/schedule.rb
@@ -1,4 +1,7 @@
 class Schedule
+  RANGE = 0..1440
+  private_constant :RANGE
+
   attr_reader :ranges
 
   def initialize
@@ -6,34 +9,23 @@ class Schedule
   end
 
   def add from, to
-    validate_range!(from, to)
-
-    range = [from, to]
-    @ranges = add_new_range(range)
+    update_ranges_with(from, to, &method(:add_new_range))
   end
 
   def remove from, to
-    validate_range!(from, to)
-
-    range = [from, to]
-    @ranges = remove_range(range)
+    update_ranges_with(from, to, &method(:remove_range))
   end
 
   private
-
-    MAX_RANGE = 1440
-
     def add_new_range new_range
       new_ranges = []
 
-      n_from = new_range[0]
-      n_to   = new_range[1]
+      n_from, n_to = new_range
 
       new_range_used = false
 
       ranges.each do |existed_range|
-        e_from = existed_range[0]
-        e_to   = existed_range[1]
+        e_from, e_to = existed_range
 
         if n_to < e_from
           new_ranges << [n_from, n_to] unless new_range_used
@@ -59,12 +51,10 @@ class Schedule
     def remove_range range_to_remove
       new_ranges = []
 
-      r_from = range_to_remove[0]
-      r_to   = range_to_remove[1]
+      r_from, r_to = range_to_remove
 
       ranges.each do |existed_range|
-        e_from = existed_range[0]
-        e_to   = existed_range[1]
+        e_from, e_to = existed_range
 
         if e_from < r_from && e_to > r_to
           new_ranges << [e_from, r_from]
@@ -81,15 +71,19 @@ class Schedule
       new_ranges
     end
 
+    def update_ranges_with(from, to)
+      validate_range!(from, to)
+
+      range = [from, to]
+
+      @ranges = yield([from, to])
+    end
+
     def validate_range! from, to
       valid_range?(from, to) || raise(ArgumentError)
     end
 
     def valid_range? from, to
-      from.is_a?(Integer) &&
-      to.is_a?(Integer) &&
-      from < to &&
-      from >= 0 &&
-      to <= MAX_RANGE
+      from < to && RANGE.cover?(from) && RANGE.cover?(to)
     end
 end
